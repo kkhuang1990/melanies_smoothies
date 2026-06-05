1c46171861d764799e6ff27303f7ff60f3b95330
Albert Huang <kkhuang1990@gmail.com>
2026-06-05 15:02:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,4 +60,5 @@ if time_to_insert:
     st.rerun()
   
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
